d5272cce881e9fd8f0a78b60ed197aa7d82e75e0
adleonard96 <alexanderleonard27@gmail.com>
2025-06-02 16:23:45 -0400

    forgot to save challenge 26

--- 26.py
+++ 26.py
@@ -1,4 +1,10 @@
-def removeDuplicates(nums) -> int:
-    return list(set(nums))
+def removeDuplicates(nums: List) -> int:
+    pointer = 0
+    for i in range(1, len(nums)):
+        if nums[pointer] < nums[i]:  
+            nums[pointer + 1] = nums[i]
+            pointer += 1
 
-print(sorted(removeDuplicates([1,1,2])))
+    return pointer + 1
+
+print(removeDuplicates([1,1,2]))
