df970d2a773d405c816e8339089dcf346cc33dca
adleonard96 <alexanderleonard27@gmail.com>
2025-08-03 18:31:38 -0400

    finished question 1578

---
+++ med/1578.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        prev_color = colors[0]
+        time_needed = 0
+        i = 0
+        j = 1
+        while j < len(colors):
+            while colors[j] == colors[i]:
+                if neededTime[j] < neededTime[i]:
+                    time_needed += neededTime[j]
+                    j += 1
+                else:
+                    time_needed += neededTime[i]
+                    i = j
+                    j += 1
+                
+                if j == len(colors):
+                    break
+            
+            i = j
+            j += 1
+    
+        return time_needed
+    
+sol = Solution()
+
+print(sol.minCost("aabaa", [1,2,3,4,1]))
