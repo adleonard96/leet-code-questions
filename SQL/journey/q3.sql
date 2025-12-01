c957c9f0d790ac1db0085928271fa4d37cfe316c
adleonard96 <alexanderleonard27@gmail.com>
2025-12-01 16:08:28 -0500

    finished Q3

---
+++ SQL/journey/q3.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    id, movie, description, rating
+from 
+    cinema
+where 
+    description != "boring"
+    and
+    id % 2 != 0
+order by rating desc
