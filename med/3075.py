29aa22b081fbc2a20f5ddade157c5189b77ac899
adleonard96 <alexanderleonard27@gmail.com>
2025-12-25 22:13:47 -0500

    3075 complete

---
+++ med/3075.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness = sorted(happiness, reverse=True)
+        total = 0
+        for i in range(k):
+            if happiness[i] - i > 0:
+                total += happiness[i] - i
+
+        return total
