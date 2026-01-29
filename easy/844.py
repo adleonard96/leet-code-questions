39021e4f56373279e9cef572785ac56267c4c500
adleonard96 <alexanderleonard27@gmail.com>
2026-01-29 16:58:45 -0500

    completed 844

---
+++ easy/844.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s_stack = []
+        t_stack = []
+        for c in s:
+            if s_stack and c == "#":
+                s_stack.pop()
+            elif c != "#":
+                s_stack.append(c)
+        for c in t:
+            if t_stack and c == "#":
+                t_stack.pop()
+            elif c != "#":
+                t_stack.append(c)
+
+        return "".join(s_stack) == "".join(t_stack)
