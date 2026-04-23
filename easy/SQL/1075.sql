f340ff81ad9833092a4bb228edc7c8d6794dd169
adleonard96 <alexanderleonard27@gmail.com>
2026-04-23 17:37:42 -0400

    finished 1075

---
+++ easy/SQL/1075.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT project_id, round(avg(experience_years) , 2) average_years from project p join employee e on e.employee_id = p.employee_id group by project_id order by project_id asc
