1463578901a48f0e48b5cf7bce990151d99ea6d7
adleonard96 <alexanderleonard27@gmail.com>
2026-05-31 13:37:58 -0400

    finished 804

---
+++ easy/804.java
@@ -0,0 +1,43 @@
+package easy;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> unique = new HashSet<>();
+        HashMap<Character, String> mp = new HashMap<>();
+        mp.put('a', ".-");
+        mp.put('b', "-...");
+        mp.put('c', "-.-.");
+        mp.put('d', "-..");
+        mp.put('e', ".");
+        mp.put('f', "..-.");
+        mp.put('g', "--.");
+        mp.put('h', "....");
+        mp.put('i', "..");
+        mp.put('j', ".---");
+        mp.put('k', "-.-");
+        mp.put('l', ".-..");
+        mp.put('m', "--");
+        mp.put('n', "-.");
+        mp.put('o', "---");
+        mp.put('p', ".--.");
+        mp.put('q', "--.-");
+        mp.put('r', ".-.");
+        mp.put('s', "...");
+        mp.put('t', "-");
+        mp.put('u', "..-");
+        mp.put('v', "...-");
+        mp.put('w', ".--");
+        mp.put('x', "-..-");
+        mp.put('y', "-.--");
+        mp.put('z', "--..");
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char letter : word.toCharArray()) {
+                sb.append(mp.get(letter));
+            }
+            unique.add(sb.toString());
+        }
+
+        return unique.size();
+    }
+}
